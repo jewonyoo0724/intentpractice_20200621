6474dd52ae148a03158465e7fb8e4c87969d732a
유제원 <skywalker0724@gmail.com>
2020-06-21 06:54:37 -0400

    메인화면에서 닉변 결과를 받아 활용

--- app/src/main/java/kr/co/namu/intentpractice_20200621/MainActivity.kt
+++ app/src/main/java/kr/co/namu/intentpractice_20200621/MainActivity.kt
@@ -1,5 +1,6 @@
 package kr.co.namu.intentpractice_20200621
 
+import android.app.Activity
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
@@ -50,6 +51,27 @@ class MainActivity : AppCompatActivity() {
             // 결과를 가지러 가는 이동은 startActvityForResult로 가야함
             // 이런 방식의 이동은 어떤 결과를 가지러 (닉네임/비번 등) 가는지 숫자로 구별해야함. e.g. 닉네임- 1000
             startActivityForResult(myIntent, 1000)
+        }
+    }
+//  어떤 종류가 되었든, 결과를 가지고 돌아올때 실행되는 함수
+    
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        
+        // 이 함수가 실행된 이유 (어떤 요청?) -> 1000 번이였는지? -> 닉네임 변경을 위해 다녀온건지?
+        
+        if (requestCode == 1000) {
+            
+            // 확인을 누른게 맞는지? -> 취소를 한게 아닌지?
+            
+            if (resultCode == Activity.RESULT_OK) {
+                
+                // 닉변 화면에서 첨부해준 newNIckName 스트링값을 저장.
+                val newNick = data?.getStringExtra("newNickName")
+                // 받아온 닉네임을 텍스트뷰에 표시
+                nickNameTxt.text = newNick
+                
+            }
         }
     }
 }
